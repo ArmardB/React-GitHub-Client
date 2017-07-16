9444f7dde1f2eb5b8e22c19478921d32852fbbe6
Armard Bellamy <Armard@Armards-MacBook.local>
2017-07-15 23:13:48 -0400

    Created Header and Github component init

---
+++ src/Github.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Github extends Component {
+  render(){
+    return(
+      <div>
+        Hey Armard, you are going to be making 6 figures within four months!
+      </div>
+    );
+  }
+}
+
+export default Github;
