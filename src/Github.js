837c5442286165198952a9de76859f86b6c24704
Armard Bellamy <Armard@Armards-MacBook.local>
2017-07-17 19:30:31 -0400

    Pulled profile data from github

--- src/Github.js
+++ src/Github.js
@@ -1,10 +1,56 @@
 import React, {Component} from 'react';
+import Profile from './components/Profile';
+import Search from './components/Search';
+
+const API ='https://api.github.com/users';
 
 class Github extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      userName: 'armardb',
+      name: '',
+      avator: '',
+      repos: '',
+      followers: '',
+      following: '',
+      homeUrl: ''
+    };
+  }
+
+  componentDidMount() {
+    this.getProfile(this.state.userName);
+  }
+
+  getProfile = (username) => {
+    let finalUrl = `${API}/${username}`;
+
+    fetch(finalUrl)
+      .then((res) => {
+        res.json();
+      }).then((data) => {
+        this.setState({
+          userName: data.login,
+          name: data.name,
+          avator: data.avatar_url,
+          repos: data.public_respos,
+          followers: data.followers,
+          following: data.following,
+          homeUrl: data.html_url,
+          notFound: data.message
+        })
+      }).catch((err) => console.log('There was a problem while fetching data: ' + err));
+  }
+
   render(){
     return(
       <div>
-        <p>You'll be making $4k monthly MRR by Jan 1, 2018!!</p>
+          <section id="card">
+            <Search />
+            <Profile />
+          </section>
       </div>
     );
   }
