320931982dd4873d68c1d68b7dea01f1d308dea1
Armard Bellamy <Armard@Armards-MacBook.local>
2017-07-17 20:08:20 -0400

    Added styling for profile display

--- src/Github.js
+++ src/Github.js
@@ -1,56 +1,56 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Profile from './components/Profile';
 import Search from './components/Search';
 
-const API ='https://api.github.com/users';
-
+const API = 'https://api.github.com/users'
 class Github extends Component {
 
   constructor(props){
     super(props);
 
     this.state = {
-      userName: 'armardb',
+      username: 'armardb',
       name: '',
-      avator: '',
+      avatar: '',
       repos: '',
       followers: '',
       following: '',
-      homeUrl: ''
+      homeURL: '',
+      notFound: ''
     };
   }
 
-  componentDidMount() {
-    this.getProfile(this.state.userName);
+  getProfile(username){
+    let finalURL = `${API}/${username}`;
+
+    fetch(finalURL)
+    .then((res) => res.json() )
+    .then((data) => {
+      this.setState({
+        username: data.login,
+        name: data.name,
+        avatar: data.avatar_url,
+        repos: data.public_repos,
+        followers: data.followers,
+        following: data.following,
+        homeURL: data.html_url,
+        notFound: data.message
+      })
+    })
+    .catch((error) => console.log('THere was a problem in fetching data'))
   }
 
-  getProfile = (username) => {
-    let finalUrl = `${API}/${username}`;
-
-    fetch(finalUrl)
-      .then((res) => {
-        res.json();
-      }).then((data) => {
-        this.setState({
-          userName: data.login,
-          name: data.name,
-          avator: data.avatar_url,
-          repos: data.public_respos,
-          followers: data.followers,
-          following: data.following,
-          homeUrl: data.html_url,
-          notFound: data.message
-        })
-      }).catch((err) => console.log('There was a problem while fetching data: ' + err));
+  componentDidMount(){
+    this.getProfile(this.state.username);
   }
 
   render(){
     return(
       <div>
-          <section id="card">
-            <Search />
-            <Profile />
-          </section>
+        <section id="card">
+          <Search searchProfile={this.getProfile.bind(this)} />
+          <Profile userData={this.state}/>
+        </section>
       </div>
     );
   }
